1c57a346f622e682f51cdada5964d8dbe878eaaa
yashkr00129 <yashkr00129@gmail.com>
2025-04-14 20:04:03 +0530

    Upload Receipts To AWS S3

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,12 +3,14 @@ import { AppSidebar } from "@/components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<main>
-				<SidebarTrigger />
-				{children}
-			</main>
-		</SidebarProvider>
+		<>
+			<SidebarProvider>
+				<AppSidebar />
+				<main className="bg-gray-50">
+					<SidebarTrigger />
+					{children}
+				</main>
+			</SidebarProvider>
+		</>
 	);
 }
